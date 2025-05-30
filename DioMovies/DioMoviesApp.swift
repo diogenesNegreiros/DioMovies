645a5648ebbf74ef9a2fe4a6211c1a4fea85f197
NEGREIROS Diogenes <diogenesdesouza@yahoo.com.br>
2025-05-30 13:38:53 -0300

    Initial Commit

---
+++ DioMovies/DioMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DioMoviesApp.swift
+//  DioMovies
+//
+//  Created by Diogenes de Souza Negreiros on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct DioMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
